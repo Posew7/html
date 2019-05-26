d55d218c8dcc207dd13ac3d78e3cbe7942b1cbd7
posew7 <posew7@gmail.com>
2019-05-26 16:01:51 -0400

    php_mysql

--- index.php
+++ index.php
@@ -2,4 +2,14 @@
 
 require_once "baglan.php";
 
+if (!isset($_GET["sayfa"])){
+    $_GET["sayfa"] = "index";
+}
+
+switch ($_GET["sayfa"]){
+        case "insert";
+            require_once "insert.php";
+        break;
+}
+
 ?>
