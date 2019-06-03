909f2d467dcd7fbac6f382a63fd80b131daae3fd
posew7 <posew7@gmail.com>
2019-06-03 10:03:27 -0400

    php_mysql

--- oku.php
+++ oku.php
@@ -1,1 +1,20 @@
 <?php require_once "header.php"; ?>
+
+<?php
+
+if (!isset($_GET["id"]) || empty($_GET["id"])){
+    header("Location:index.php");
+    exit;
+}
+
+$sorgu = $db->prepare("SELECT * FROM dersler WHERE id = ?");
+$sorgu->execute([$_GET["id"]]);
+$ders = $sorgu->fetch(PDO::FETCH_ASSOC);
+print_r($ders);
+
+if (!$ders){
+    header("Location:index.php");
+    exit;
+}
+
+?>
